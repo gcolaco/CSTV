5f5113d1a36e1015e9dcbc35c9c0db106e8914a0
Gustavo Colaço <gustavo.colaco@hotmail.com>
2023-02-26 20:20:56 -0300

    Feature: Adding unit tests

--- CSTVTests/CSTVTests.swift
+++ CSTVTests/CSTVTests.swift
@@ -9,28 +9,44 @@ import XCTest
 @testable import CSTV
 
 final class CSTVTests: XCTestCase {
+    private let sut = NetworkingMock()
+    
+    func testNeworking() {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+        let emptyUpcommingArray = sut.upcommingMatches
+        XCTAssertTrue(emptyUpcommingArray.isEmpty, "Array is empty")
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let emptyRunningArray = sut.upcommingMatches
+        XCTAssertTrue(emptyRunningArray.isEmpty, "Array is empty")
+        
+        NetworkManager.shared.getUpcomingMatches(page: 1) { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let matches):
+                self.sut.upcommingMatches.append(contentsOf: matches)
+                XCTAssertTrue(!self.sut.upcommingMatches.isEmpty, "Array is empty")
+            case .failure(let error):
+                print("ERROR: \(error.rawValue)")
+            }
+        }
+        
+        NetworkManager.shared.getRunningMatches { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let matches):
+                self.sut.runningMatches.append(contentsOf: matches)
+                XCTAssertTrue(!self.sut.runningMatches.isEmpty, "Array is empty")
+            case .failure(let error):
+                print("ERROR: \(error.rawValue)")
+            }
         }
     }
+    
+}
 
+
+class NetworkingMock {
+    var upcommingMatches: [Matches] = []
+    var runningMatches: [Matches] = []
+    
 }
